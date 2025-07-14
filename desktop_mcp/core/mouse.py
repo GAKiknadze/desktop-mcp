1abc6f5fa9438d99bbeec76b8f2754b14c576e31
GAKiknadze <gakiknadze@yandex.ru>
2025-07-15 02:00:40 +0300

    methods renaming

--- desktop_mcp/core/mouse.py
+++ desktop_mcp/core/mouse.py
@@ -2,42 +2,42 @@ from typing import Protocol, Tuple
 
 
 class MouseController(Protocol):
-    def mouse_position(self) -> Tuple[int, int]:
+    def position(self) -> Tuple[int, int]:
         "Current mouse position x and y"
         ...
 
-    def mouse_move_to(self, x: int, y: int):
+    def move_to(self, x: int, y: int) -> None:
         """Move mouse to XY coordinates"""
         ...
 
-    def mouse_move_rel(self, x: int, y: int):
+    def move_rel(self, x: int, y: int) -> None:
         """Move mouse relative to its current position"""
         ...
 
-    def mouse_drag_to(self, x: int, y: int):
+    def drag_to(self, x: int, y: int) -> None:
         """Drag mouse to XY"""
         ...
 
-    def mouse_drag_rel(self, x: int, y: int):
+    def drag_rel(self, x: int, y: int) -> None:
         """Drag mouse relative to its current position"""
         ...
 
-    def mouse_right_click(self, x: int = None, y: int = None):
+    def right_click(self, x: int = None, y: int = None) -> None:
         """Mouse right click"""
         ...
 
-    def mouse_left_click(self, x: int = None, y: int = None):
+    def left_click(self, x: int = None, y: int = None) -> None:
         """Mouse left click"""
         ...
 
-    def mouse_left_double_click(self, x: int = None, y: int = None):
+    def left_double_click(self, x: int = None, y: int = None) -> None:
         """Mouse left double click"""
         ...
 
-    def mouse_middle_click(self, x: int = None, y: int = None):
+    def middle_click(self, x: int = None, y: int = None) -> None:
         """Mouse middle click"""
         ...
 
-    def mouse_scroll(self, amount_to_scroll: int, x: int = None, y: int = None):
+    def scroll(self, amount_to_scroll: int, x: int = None, y: int = None) -> None:
         """Positive scrolling will scroll up, negative scrolling will scroll down"""
         ...
